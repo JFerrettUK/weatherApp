b6c4f2982154b7b8888acdcc5dbf9ee563ddd038
JFerrettUK <jferrettuk@gmail.com>
2023-02-13 12:06:04 +0000

    Webpack alterations

--- capitalizeWord.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable no-param-reassign */
-export default function capitalizeWord(string) {
-  return string[0].toUpperCase() + string.slice(1).toLowerCase();
-}
